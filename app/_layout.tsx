b9aecd139ae6e0f1e6e5cd794eafcbadc1d252f5
Lowie <lowie.klaasen@student.ap.be>
2025-02-12 14:37:40 +0100

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Tabs } from 'expo-router';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function RootLayout() {
+  return (
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Map',
+            tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home" color={color} size={size} />
+          }}
+        />
+      </Tabs>
+  )
+}
