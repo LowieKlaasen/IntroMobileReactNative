8b6936c6051c2b9cb36a56ec4132750ccadcfdac
Lowie <lowie.klaasen@student.ap.be>
2025-02-12 15:03:05 +0100

    Succesful API call

--- app/list.tsx
+++ app/list.tsx
@@ -1,5 +1,21 @@
-const List = () => {
+import { View } from "react-native"
 
+const List = () => {
+    getLocationsFromApi();
+    return (<View>List</View>)
 }
+
+const getLocationsFromApi = () => {
+    return fetch('https://sampleapis.assimilate.be/ufo/sightings')
+      .then(response => response.json())
+      .then(json => {
+        console.log("Api call succeeded")
+        console.log(json);
+        return json.movies;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
 export default List;
