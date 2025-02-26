7ced5d92a1dba06adfe90583eb8306cbeef6dfc3
Lowie <lowie.klaasen@student.ap.be>
2025-02-26 17:43:05 +0100

    Async Storage in test component

--- app/test.tsx
+++ app/test.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface Student {
@@ -8,31 +8,49 @@ interface Student {
   age: number;
 }
 
-const storeData = async () => {
-  let student: Student = {
-    name: "John Doe",
-    age: Math.floor(Math.random() * 10) + 18,
-  };
-  await AsyncStorage.setItem("randomStudent", JSON.stringify(student));
-};
-
-const getData = async () => {
-  const value: string = await AsyncStorage.getItem("randomStudent");
-  if (value !== null) {
-    let student: Student = JSON.parse(value);
-    console.log(student.name + " is " + student.age + " years old");
-  } else {
-    console.log("No Data found");
-  }
-};
 const Test = () => {
+  const [studentName, setStudentName] = useState("");
+
+  const storeData = async () => {
+    let student: Student = {
+      name: studentName,
+      age: Math.floor(Math.random() * 10) + 18,
+    };
+    await AsyncStorage.setItem("randomStudent", JSON.stringify(student));
+
+    setStudentName("");
+  };
+
+  const getData = async () => {
+    const value = await AsyncStorage.getItem("randomStudent");
+
+    if (value !== null) {
+      let student: Student = JSON.parse(value);
+      console.log(`${student.name} is ${student.age} years old`);
+    } else {
+      console.log("No Data found");
+    }
+  };
+
   return (
     <View>
-      <Text>AsyncStorage</Text>
+      <Text>AsyncStorage Example</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter name..."
+        value={studentName}
+        onChangeText={setStudentName}
+      />
+
       <Button title="Store Data" onPress={storeData} />
       <Button title="Load Data" onPress={getData} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {},
+});
 
 export default Test;
