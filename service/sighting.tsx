26eea8ac3b7e69adfd73785200c84d6e4559b9fe
Lowie <lowie.klaasen@student.ap.be>
2025-03-15 11:18:56 +0100

    cleanup

--- service/sighting.tsx
+++ service/sighting.tsx
@@ -1,4 +1,3 @@
-// sighting.ts
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ISighting from "@/app/interfaces/ISighting";
